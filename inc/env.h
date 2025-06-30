9d6d7add3fd9fbcdad617786eda0c61598fc5b2c
Youssef <youssef.itani.11.11@gmail.com>
2025-06-30 21:10:20 +0300

    organized env

--- inc/env.h
+++ inc/env.h
@@ -6,7 +6,7 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 00:52:44 by yitani            #+#    #+#             */
-/*   Updated: 2025/06/29 05:15:09 by yitani           ###   ########.fr       */
+/*   Updated: 2025/06/30 18:54:42 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,20 @@ typedef struct	s_env
     struct s_env	*next;
 }	t_env;
 
-void	env_init(t_env **env_list, char **envp); // initializes the list of env variables
+void	env_init(t_env **env_list, char **envp); // initializes the list of env variables 
+char	*get_env_value(t_env *env, char *key); // searches for environment value by the key
+void	handle_new_key(t_env *env, char *key, char *value); // |
+void	set_env_value(t_env *env, char *key, char *value);	// |	related
+void	unset_env_value(t_env **env, char *key);
+int		valid_entries_count(t_env *env);
+char	**to_envp(t_env *env);
+
+// expand $VAR into its value 👍🏼
+
+char	*expand_token_value_1(char *value, t_env *env, int *i);
+char	*expand_token_value_1_5(char *dest, const char *to_append);
+char	*handle_dollar_expansion(char *result, char *value, t_env *env, int *i);
+char	*expand_token_value_final(char *value, t_env *env);
+t_token	*expand_variables_in_tokens(t_token *tokens, t_env *env);
 
 #endif
--- src/env/parsed_environment.c
+++ src/env/parsed_environment.c
@@ -6,13 +6,13 @@
 /*   By: yitani <yitani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 13:06:38 by yitani            #+#    #+#             */
-/*   Updated: 2025/06/29 20:29:48 by yitani           ###   ########.fr       */
+/*   Updated: 2025/06/30 18:46:07 by yitani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	valid_entries_count(t_env *env)
+int	valid_entries_count(t_env *env)
 {
 	int		count;
 	t_env	*current;
@@ -54,102 +54,4 @@ char	**to_envp(t_env *env)
 	}
 	envp[i] = NULL;
 	return(envp);
-}
-
-// var[0] = var_name
-// var[1] = var_value
-
-char	*expand_token_value_1(char *value, t_env *env, int *i)
-{
-	int		start;
-	char	*var_name;
-	char	*var_value;
-
-	start = i[0];
-	while (ft_isalnum(value[i[0]] || value[i[0]] == '_'))
-		i[0]++;
-	var_name = ft_substr(value, i[1], i[0] - i[1]);
-	var_value = get_env_value(env, var_name);
-	if (!var_value)
-		var_value = ft_strdup("");
-	else
-		var_value = ft_strdup(var_value);
-	free(var_name);
-	return(var_value);
-}
-
-char	*expand_token_value_1_5(char *dest, const char *to_append)
-{
-	char	*new;
-
-	new = ft_strjoin(dest, to_append);
-	free(dest);
-	return (new);
-}
-
-char	*handle_dollar_expansion(char *result, char *value, t_env *env, int *i)
-{
-	char *expanded;
-	char *temp;
-
-	if (ft_isalnum(value[i[0]]) || value[i[0]] == '_')
-	{
-		expanded = expand_token_value_1(value, env, i);
-		temp = result;
-		result = ft_strjoin(result, expanded);
-		free(temp);
-		free(expanded);
-	}
-	else
-	{
-		result = expand_token_value_1_5(result, "$");
-	}
-	return (result);
-}
-
-char	*expand_token_value_final(char *value, t_env *env)
-{
-	int		i[2];
-	char	*result;
-	char	*temp;
-	char	*expanded;
-
-	i[0] = 0;
-	result = ft_strdup("");
-	while (value[i[0]])
-	{
-		if (value[i[0]++] == '$')
-			result = handle_dollar_expansion(result, value, env, i);
-		else
-		{
-			i[1] = i[0];
-			while (value[i[0]] && value[i[0]] != '$')
-				i[0]++;
-			expanded = ft_substr(value, i[1], i[0] - i[1]);
-			temp = result;
-			result = ft_strjoin(result, expanded);
-			free(temp);
-			free(expanded);
-		}
-	}
-	return (result);
-}
-
-t_token	*expand_variables_in_tokens(t_token *tokens, t_env *env)
-{
-	t_token	*curr;
-	char	*expanded;
-
-	curr = tokens;
-	while (curr)
-	{
-		if (curr->type == TOKEN_WORD && curr->single_quotes == 0)
-		{
-			expanded = expand_token_value_final(curr->value, env);
-			free(curr->value);
-			curr->value = expanded;
-		}
-		curr = curr->next;
-	}
-	return (tokens);
 }
